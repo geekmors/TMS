2f78e5e6a7999800a27e57d50fa2942d1940dbb4
amayaKarl <aakarl32@gmail.com>
2021-10-05 09:48:10 -0600

    added navbar and sidenav

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!--[BEGIN meta definitions]-->
     <meta charset="UTF-8">
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--[END meta definitions]-->
+    
     <title>{{config('app.name')}}</title>
+    
+    <!--[BEGIN css includes]-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/nav.css')}}" media="screen"/>
+    <!--[END css include]-->
+    
+    <!--[BEGIN top level JS script includes]-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="{{asset('js/AppDB.js')}}" type="text/javascript"></script>
+    <!--[END top level JS script includes]-->
 </head>
--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,5 +1,24 @@
 <header id="header">
-    <div class="navbar">
-        
+<!-- Top Nav bar with ksm logo and user icon -->
+    <div id="top-nav" class="d-flex">
+      <div class="mr-auto align-self-center pl-5">
+        <a href="/"><img src="{{$logo}}" style="width:18em;"></a>
+      </div>
+
+      <ul class="nav ml-auto align-self-center pr-5">
+        <li class="nav-item">
+          <a class="nav-link" href=""><i class="fa fa-bell" id="notifications"></i></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown"><i class="fa fa-user" id="user-icon"></i></a>
+          <div class="dropdown-menu" id="profile-dropdown">
+            <a class="dropdown-item" href=""><i class="fa fa-bell mr-2"></i>ACTION HERE</a>
+            <a class="dropdown-item" href="/settings"><i class="fa fa-cog mr-2"></i>Settings</a>
+            <a class="dropdown-item" href="/signout"><i class="fa fa-sign-out mr-2"></i>Sign Out</a>
+          </div>
+        </li>
+      </ul>
     </div>
+
+    @include('inc.sidenav')
 </header>
