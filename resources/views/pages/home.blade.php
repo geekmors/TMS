8551523c700b21b5c8b86025166a6bfa3ebb6ab3
amayaKarl <aakarl32@gmail.com>
2021-10-06 19:22:11 -0600

    message irrelevant

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="logo" >
-    <img style="width:300px" src="{{$logo}}" alt="Company Logo">
-    
-</div>
 <h2>Hello world!</h2>
 @endsection
